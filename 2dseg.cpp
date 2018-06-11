cf9d75d76a0b192beeabb7342e258f6fa69ea538
Rishabh Paul <rishabhpaul77@gmail.com>
2018-06-11 23:13:45 +0530

    Updating Comments and Documentation

--- 2dseg.cpp
+++ 2dseg.cpp
@@ -5,7 +5,7 @@
 *	Input : n * m 2D Array (Standard Input)
 *	Query : sum of sub-rectangle whose upper-left corner is<r1 ,c1> and lower-right corner is<r2 ,c2> (Standard Output)
 *	Update : a[r][c] = x 
-*	Constraints : 0 <= r < n , 0 <= c < m , -1e9 <= x <= 1e9
+*	Constraints : 1 <= n <= 1000 ;1 <= m <= 1000 ; 0 <= r < n ; 0 <= c < m ; -1e3 <= x <= 1e3
 *	Space : 4mn
 * 	Query-Time : O(lgn(n) * lg(m))	, where lg is logarithm base 2
 *	0-based indexing everywhere
@@ -78,14 +78,25 @@ void update(std::vector<std::vector<int>> &seg ,std::vector<std::vector<int>> &a
 	update(seg ,a ,RIGHT(i) ,q + 1 ,e ,r ,c ,x);
 	for(int j = 0 ; j < 2 * m ; j++)	seg[i][j] = seg[LEFT(i)][j] + seg[RIGHT(i)][j];
 }
+
 int main() {
 	int Q ,t ,r1 ,c1 ,r2 ,c2;
+	//Input
 	scanf("%d %d" ,&n ,&m);
 	std::vector<std::vector<int>> a(n ,std::vector<int>(m)) ,seg(2 * n ,std::vector<int>(2 * m , 0));
 	for(int i = 0 ; i < n ; i++)	
 		for(int j = 0 ; j < m ; j++)	scanf("%d" ,&a[i][j]);
-	build(seg ,a , 0 ,0 ,n - 1);
 	scanf("%d" ,&Q);
+
+	build(seg ,a , 0 ,0 ,n - 1);	//Building 2D Segment Tree in O(4mn) Time
+	
+	/*
+		Queries are of two types
+		0 r1 c1 r2 c2 : print sum of sub-rectangle with upper-left corner <r1 ,c1> and lower-right corner <r2 ,c2>
+		1 r c x : update a[r][c] = x;
+		Constraints : 0 <= r1 <= r2 < n ; 0 <= c1 <= c2 < m
+	*/
+
 	while(Q--) {
 		scanf("%d %d %d %d" ,&t ,&r1 ,&c1 ,&r2);
 		if(t == 0) {
